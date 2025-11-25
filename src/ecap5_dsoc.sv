7416edd8ed649c5f2b13226f552d4af3f7364de4
Clément Chaine <clement.chaine25@gmail.com>
2025-11-25 23:47:43 +0100

    Add helloworld bootloader

--- src/ecap5_dsoc.sv
+++ src/ecap5_dsoc.sv
@@ -113,7 +113,7 @@ ecap5_dwbuart uart_inst (
 
 ecap5_dwbmem_bram #(
   .ENABLE_PRELOADING (1),
-  .PRELOAD_HEX_PATH ("/home/ubuntu/ecap5/firmware/output.hex")
+  .PRELOAD_HEX_PATH ("/tmp/build/firmware/helloworld.load")
 ) bram_inst (
   .clk_i (clk_i),
   .rst_i (rst_i),
